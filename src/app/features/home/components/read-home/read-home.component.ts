d16f387f18982ca4a1cecdd7f4fdd1e0f6218b30
FrancisFrancois <francisfrancois9189@gmai.com>
2023-12-30 01:06:31 +0100

    CRUD Home, Create Delete, Update et Read en cours

--- src/app/features/home/components/read-home/read-home.component.ts
+++ src/app/features/home/components/read-home/read-home.component.ts
@@ -3,35 +3,53 @@ import { ReadHome } from '../../models/home';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HomeService } from '../../services/home.service';
 import { ReadUser } from 'src/app/features/user/models/user';
+import { UserService } from 'src/app/features/user/services/user.service';
 
 @Component({
   selector: 'app-read-home',
   templateUrl: './read-home.component.html',
   styleUrls: ['./read-home.component.scss']
 })
-export class ReadHomeComponent {
 
-  readHome : ReadHome | undefined;
+export class ReadHomeComponent {
+  readHome: ReadHome | undefined;
+  userName: string[] = [];
 
   constructor(
-    private _activeRoute : ActivatedRoute, 
-    private _homeService : HomeService,
-    private _router : Router,
-    ) {
-    let homeId = +this._activeRoute.snapshot.params['id'];
+    private _activeRoute: ActivatedRoute, 
+    private _homeService: HomeService,
+    private _router: Router,
+    private _userService: UserService
+  ) {
+    this.ngOnInit();
+  }
+
+  ngOnInit(): void {
+    const homeId = +this._activeRoute.snapshot.params['id'];
     
     this._homeService.getById(homeId).subscribe({
-      next : (response) => {
+      next: (response) => {
         this.readHome = response;
         console.log("Récupération de la maison : ", response);
+        this.loadUser();
       },
-      error : (error) => {
+      error: (error) => {
         console.error("Erreur lors de la recuperation de la maison : ", error);
         this._router.navigateByUrl('/not-found');
       },
-      complete : () => {
+      complete: () => {
         console.log("Récupération de la maison terminée");
       }
     });
+  }
+
+  loadUser() {
+    if (this.readHome && this.readHome.users) {
+      this.readHome.users.forEach(user => {
+        this._userService.getById(user.user_Id).subscribe(userData => {
+          this.userName[user.user_Id] = `${userData.firstname} ${userData.lastname}`; 
+        });
+      });
+    }
   }
 }
